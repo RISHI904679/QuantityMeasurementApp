a7e5ae25c920214dec15d04d52890d4124340ca9
RISHI904679 <trishiraj2006@gmail.com>
2026-04-27 14:33:55 +0530

    UC2: Feet and Inches equality

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,6 +1,6 @@
 public class QuantityMeasurementApp {
 
-    // Inner class for Feet
+    // 🔹 Feet Class
     static class Feet {
         private final double value;
 
@@ -10,22 +10,52 @@ public class QuantityMeasurementApp {
 
         @Override
         public boolean equals(Object obj) {
-
             if (this == obj) return true;
-
             if (obj == null || getClass() != obj.getClass()) return false;
 
             Feet other = (Feet) obj;
-
             return Double.compare(this.value, other.value) == 0;
         }
     }
 
+    // 🔹 Inches Class (NEW for UC2)
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // 🔹 Method for Feet equality
+    public static boolean checkFeetEquality(double a, double b) {
+        Feet f1 = new Feet(a);
+        Feet f2 = new Feet(b);
+        return f1.equals(f2);
+    }
+
+    // 🔹 Method for Inches equality
+    public static boolean checkInchesEquality(double a, double b) {
+        Inches i1 = new Inches(a);
+        Inches i2 = new Inches(b);
+        return i1.equals(i2);
+    }
+
     public static void main(String[] args) {
 
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+        // Feet check
+        System.out.println("Feet Equal: " + checkFeetEquality(1.0, 1.0));
 
-        System.out.println("Equal: " + f1.equals(f2));
+        // Inches check
+        System.out.println("Inches Equal: " + checkInchesEquality(1.0, 1.0));
     }
 }
